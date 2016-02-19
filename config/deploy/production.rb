ab5972218a52c50da349bf9a8ec86d8739764f2c
heysabbinah <dubysound@hotmail.com>
2016-02-19 16:45:06 +0100

    add config.assets.precompile = ['*.js', '*.css', '*.css.erb'] and change false to true in compile asset in config/envir/prod.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -19,7 +19,7 @@ role :db,  %w{deploy@vps233871.ovh.net}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'vps233871.ovh.net', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '51.255.201.76', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
